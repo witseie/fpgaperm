591562aea2a1eee36f1df2810a0f95f4ad5c720d
Yaniv S <52028054+Yaniv42@users.noreply.github.com>
2023-04-10 18:01:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,12 +3,9 @@ export PATH=$PATH:/usr/sbin
 export FPGA_PERM_PATH=/home/centos/FPGA_perm
 export LC_ALL="C"
 
-git clone https://github.com/aws/aws-fpga.git $AWS_FPGA_REPO_DIR
+pip3 install --user statsmodels
 
-git clone https://github.com/witseie/fpgaperm.git --no-checkout --single-branch $FPGA_PERM_PATH/tmp
-mv $FPGA_PERM_PATH/tmp/.git $FPGA_PERM_PATH/
-rm -rf $FPGA_PERM_PATH/tmp
-(cd $FPGA_PERM_PATH/ && git reset --hard HEAD)
+git clone https://github.com/aws/aws-fpga.git $AWS_FPGA_REPO_DIR
 
 source $AWS_FPGA_REPO_DIR/vitis_setup.sh
 source $AWS_FPGA_REPO_DIR/vitis_runtime_setup.sh
